1fb0d6a31aaf6093dde92b87537bdcbcc4d26986
jesuggc <jesugo11@ucm.es>
2023-10-22 02:14:57 +0200

    some more js ex done

---
+++ javascript/advanced/exercise5/index.js
@@ -0,0 +1,16 @@
+
+
+function concatenar() {
+    let finalString = ""
+    for (let i = 1; i < arguments.length; i++) {
+        finalString = finalString.concat(arguments[i], arguments[0])
+    }
+    return finalString
+}
+
+
+console.log(concatenar()) // Devuelve la cadena vacía
+console.log(concatenar("-")) // Devuelve la cadena vacía
+console.log(concatenar("-","uno")) // Devuelve “uno-”
+console.log(concatenar("-","uno","dos","tres")) // Devuelve “uno-dos-tres-”
+
